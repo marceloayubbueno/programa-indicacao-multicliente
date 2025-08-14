08127541879a42e99b49717dafa784d8c9b98cb9
marceloayubbueno <marcelo_ayub@hotmail.com>
2025-08-14 13:59:09 -0300

    feat: implementar DTO robusto e funcional para WhatsApp Company Header

--- server/src/whatsapp/dto/create-company-header.dto.ts
+++ server/src/whatsapp/dto/create-company-header.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsBoolean, IsOptional, IsNotEmpty } from 'class-validator';
+import { IsString, IsBoolean, IsOptional } from 'class-validator';
 
 export class CreateCompanyHeaderDto {
   @IsString()
@@ -6,85 +6,35 @@ export class CreateCompanyHeaderDto {
   clientId?: string;
 
   companyInfo: {
-    @IsString()
-    @IsNotEmpty()
     name: string;
-    
-    @IsString()
-    @IsOptional()
     description?: string;
-    
-    @IsString()
-    @IsOptional()
     website?: string;
-    
-    @IsString()
-    @IsOptional()
     phone?: string;
-    
-    @IsString()
-    @IsOptional()
     email?: string;
-    
-    @IsString()
-    @IsOptional()
     address?: string;
   };
 
   socialMedia: {
-    @IsString()
-    @IsOptional()
     instagram?: string;
-    
-    @IsString()
-    @IsOptional()
     facebook?: string;
-    
-    @IsString()
-    @IsOptional()
     linkedin?: string;
-    
-    @IsString()
-    @IsOptional()
     whatsapp?: string;
   };
 
   headerConfig: {
-    @IsBoolean()
     enabled: boolean;
-    
-    @IsString()
-    @IsNotEmpty()
     separator: string;
-    
-    @IsString()
-    @IsOptional()
     customText?: string;
   };
 
   activeFields: {
-    @IsBoolean()
     description: boolean;
-    
-    @IsBoolean()
     website: boolean;
-    
-    @IsBoolean()
     phone: boolean;
-    
-    @IsBoolean()
     email: boolean;
-    
-    @IsBoolean()
     instagram: boolean;
-    
-    @IsBoolean()
     facebook: boolean;
-    
-    @IsBoolean()
     linkedin: boolean;
-    
-    @IsBoolean()
     whatsapp: boolean;
   };
 }
